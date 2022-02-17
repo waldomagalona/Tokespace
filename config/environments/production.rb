0ad0423c845238b1a6a678d59f2b433aed868e1f
waldomagalona <waldomagalona@gmail.com>
2022-02-17 12:00:08 +0800

    fix devise mailer production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -118,13 +118,15 @@ Rails.application.configure do
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "tokespace.herokuapp.com" }
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => "tokespace.herokuapp.com", :protocol => 'http' }
   config.action_mailer.smtp_settings = {
     user_name: Rails.application.credentials.gmail[:email],
     password: Rails.application.credentials.gmail[:password],
     domain: 'gmail.com',
     address: 'smtp.gmail.com',
-    port: '587',
+    port: 587,
     authentication: :plain,
     enable_starttls_auto: true
   }
