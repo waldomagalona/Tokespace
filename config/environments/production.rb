d1372662af53abbca53d808b61ce41823357b55e
waldomagalona <waldomagalona@gmail.com>
2022-02-17 11:33:36 +0800

    fix mailers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,7 +15,19 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_url_options = { :host => 'myapp.herokuapp.com', :protocol => 'http' }
 
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'gmail.com',
+    user_name:            Rails.application.credentials.gmail[:email],
+    password:             Rails.application.credentials.gmail[:email],
+    authentication:       'plain'
+  }
   # Ensures that a master key has been made available in either ENV["RAILS_MASTER_KEY"]
   # or in config/master.key. This key is used to decrypt credentials (and other encrypted files).
   config.require_master_key = true
